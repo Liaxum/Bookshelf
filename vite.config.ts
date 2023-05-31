209c101308f8d3156295cfc352f0f699e133f975
Romain <texier.r@gmail.com>
2023-05-31 14:49:53 +0200

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,53 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}'],
+        cleanupOutdatedCaches: false
+      },
+
+      base: '/',
+      srcDir: 'src/service-worker',
+      manifest: {
+        name: 'Bookshelf',
+        short_name: 'BK',
+        description: 'My Awesome Bookshelf App',
+        theme_color: '#ffffff',
+        display: 'minimal-ui',
+        publicPath: '/public',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icon.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ]
+      }
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
+
